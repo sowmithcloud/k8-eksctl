56dcef1125c614f51f5304df2518b9ff5fe06596
sowmithcloud <sowmithcloud@gmail.com>
2024-04-09 18:00:20 +0530

    eksctl

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.31.0" # AWS provider version, not terraform version
+      version = "5.43.0" # AWS provider version, not terraform version
     }
   }
 

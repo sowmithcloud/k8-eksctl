1f1a1b6ae44d73715e72d5e6b4aade40b093945d
sowmithcloud <sowmithcloud@gmail.com>
2024-04-10 17:16:03 +0530

    eksctl

--- workstation.tf
+++ workstation.tf
@@ -24,9 +24,9 @@ resource "aws_security_group" "allow_eksctl" {
 
   ingress {
     description = "all ports"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
